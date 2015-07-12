3daed49082e3167002d48c044ae91f01ef637438
mechaman <julien.hoachuck@gmail.com>
2015-07-12 00:38:20 -0700

    Fixed Bug... No more error.

--- HPOptim/clean_up.sh
+++ HPOptim/clean_up.sh
@@ -4,18 +4,17 @@
 file1="0"
 file2="0"
 
-find . -maxdepth 1 -type f -name "spearmint.*" 2>/dev/null | grep -q . && file1="1"
+find /data/db/ -maxdepth 1 -type f -name "spearmint.*" 2>/dev/null | grep -q . && file1=1
 if [ $file1 -eq 1 ]; then
 	rm /data/db/spearmint.*
 	echo "Removed spearmint.*"
 fi
 
-find . -maxdepth 1 -type f -name "mongod.lock" 2>/dev/null | grep -q . && file2="1"
+find /data/db/ -maxdepth 1 -type f -name "mongod.lock" 2>/dev/null | grep -q . && file2=1
 if [ $file2 -eq 1 ]; then
 	rm /data/db/mongod.lock
 	echo "Removed mongod.lock"
 fi
 
 killall mongod
-mongod
 

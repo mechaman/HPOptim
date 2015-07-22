051b96b2092b23d0f461b0ed68ea7286c5a332df
mechaman <julien.hoachuck@gmail.com>
2015-07-22 15:06:42 -0700

    Fixed Issue, replaced hardcoded paths to torch directory with paths taken from /home/toor/Desktop/mongodb/bin:/home/toor/torch/install/bin:/home/toor/torch/install/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games variable

--- HPOptim/setup.sh
+++ HPOptim/setup.sh
@@ -1,6 +1,9 @@
 # Author: Julien Hoachuck
 # Copyright 2015, Julien Hoachuck, All rights reserved.
 
+temp=$(echo $PATH | grep -o ":/[a-zA-Z0-9_/]*/torch/install/bin:")
+TorchPath=$(echo $temp | grep -o "/.*/torch")
+
 luajit -b ../model.lua model.o
-gcc -w -c -Wall -Wl,-E -fpic cluaf.c -lluajit -lluaT -lTH -lm -ldl -I/home/toor/torch/install/include -L/usr/lib/x86_64-linux-gnu/
-gcc -shared cluaf.o model.o -L/home/toor/torch/install/lib -lluajit -lluaT -lTH -lm -ldl -Wl,-E -o libcluaf.so
+gcc -w -c -Wall -Wl,-E -fpic cluaf.c -lluajit -lluaT -lTH -lm -ldl -I$TorchPath/install/include -L/usr/lib/x86_64-linux-gnu/
+gcc -shared cluaf.o model.o -L$TorchPath/install/lib -lluajit -lluaT -lTH -lm -ldl -Wl,-E -o libcluaf.so
